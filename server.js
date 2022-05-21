b7b13da79e48d0a100edea95b68f44424817ed9a
Haley Telega <haleytelega@outlook.com>
2022-05-21 15:10:34 -0400

    added Model

--- server.js
+++ server.js
@@ -21,9 +21,12 @@ const sess = {
 
 app.use(session(sess));
 
-const helpers = require('./utils/helpers');
+// const helpers = require('./utils/helpers');
 
-const hbs = exphbs.create({ helpers });
+// const hbs = exphbs.create({ helpers });
+
+
+const hbs = exphbs.create({ });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
@@ -32,7 +35,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(require('./controllers/'));
+// app.use(require('./controllers/'));
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
